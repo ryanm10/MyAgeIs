7beae5a223e266610f5d40a15266c0fc54af0dc7
Ryan <ryan.milton@patelhs.org>
2020-09-27 21:43:33 +0000

    Hello, thanks for checking out my project. Within this, I am 15 years old and so I put 0403 as my set birthday. As a result, it printed I am still 15 years old as my birthday is not on September 21st. Thanks! :)

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int myBirthday = 0403;
+    int todaysDate = 921;
+
+    if (myBirthday == todaysDate) {
+      myAge = myAge + 1;
+      System.out.println("I am " + myAge  + " years old." );
+      
+    }
+    else {
+      System.out.println("I am still " + myAge + " years old." );
+    }
   }
 }
